ad90544ac60286bffb0afac1a129eb4d3a32d5d6
Kyle Moore <kmoore@gradle.com>
2021-11-24 11:40:30 -0800

    Improved samples for jacoco aggregation

--- subprojects/docs/src/samples/java/jvm-multi-project-with-code-coverage-distribution/kotlin/application/build.gradle.kts
+++ subprojects/docs/src/samples/java/jvm-multi-project-with-code-coverage-distribution/kotlin/application/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("myproject.java-conventions")
     application
-    id("jacoco-report-aggregation")
+    id("jacoco-report-aggregation") // <1>
 }
 
 dependencies {
@@ -13,7 +13,7 @@ application {
     mainClass.set("org.gradle.sample.Main")
 }
 
-// Make JaCoCo aggregate report generation part of the 'check' lifecycle phase
+// Optional: make JaCoCo aggregate report generation part of the 'check' lifecycle phase
 tasks.check {
     dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
 }
--- subprojects/docs/src/samples/java/jvm-multi-project-with-code-coverage-standalone/kotlin/code-coverage-report/build.gradle.kts
+++ subprojects/docs/src/samples/java/jvm-multi-project-with-code-coverage-standalone/kotlin/code-coverage-report/build.gradle.kts
@@ -8,18 +8,18 @@ repositories {
 }
 
 dependencies {
-    jacocoAggregation(project(":application"))
+    jacocoAggregation(project(":application")) // <1>
 }
 
 reporting {
     reports {
-        val testCodeCoverageReport by creating(JacocoCoverageReport::class) {
+        val testCodeCoverageReport by creating(JacocoCoverageReport::class) { // <2>
             testType.set(TestType.UNIT_TESTS)
         }
     }
 }
 
-// Make JaCoCo aggregate report generation part of the 'check' lifecycle phase
+// Optional: make JaCoCo aggregate report generation part of the 'check' lifecycle phase
 tasks.check {
     dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
 }
